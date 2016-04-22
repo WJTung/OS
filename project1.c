842d93cab24fbfb34b83a031ec90885d3240567f
Chih Yo Hsieh <Chih Yo Hsieh>
2016-04-23 01:35:43 +0800

    correct project1.c

--- project1.c
+++ project1.c
@@ -1,36 +1,82 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sched.h>
 
 int main(){
 	int execute[];
 	int fork[];
-	int i, j;
+	int time_count, fork_count;
 	pid_t pid;
 	int total_time;
-	
-	//一開始main先設自己priority是1
-	for(i = 0; i < N; i ++){
-		if(i == fork[j]){
-			j++;
+	fork_count = 0;
 
-			pid=fork();   
+	//set CPU
+	cpu_set_t cpu_mask;
+	CPU_ZERO(&cpu_mask);
+	CPU_SET(0, &cpu_mask);
+
+	if(sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask) != 0){
+		perror("sched_setaffinity error");
+		exit(EXIT_FAILURE);
+	}
+
+	//raise the priority
+	struct sched_param param;
+	param.sched_priority = 50;
+	//I'm not sure about how high param.sched_priority should go
+	//Just want to make sure it won't get preempted by other processes on its CPU
+	if(sched_setscheduler(pid, SCHED_FIFO, & param) != 0) {
+		perror("sched_setscheduler error");
+		exit(EXIT_FAILURE);  
+	}
+
+	for(time_count = 0; time_count < total_time; time_count++){
+		//fork the processes that are ready at time_count
+		while(time_count == fork[fork_count].ready_t){
+
+			pid = fork();
+		    //TODO: connect the pid with the process name
 		    if(pid < 0)   
 		        printf("error in fork!");   
 		    else if(pid == 0) {  
-		        //child process child exec的程式開頭會把自己設成4
-		        //跑完一時間單位他會把main設回1
-		        exec();
+		        char exec_time[10];
+				sprintf(exec_time, "%d", fork[fork_count].exec_t);
+		        if(execlp("./process", "process", fork[fork_count].name, exec_time, (char *)NULL) < 0){
+					perror("execlp error");
+					exit(EXIT_FAILURE);
+		        }
 		    }  
-		    else{  
-				//parent process
-		    }  
+			
+			fork_count++;
 		}
-		然後main把前一秒執行的process priority也設回4
-		//下一秒要執行的(execute[i])設成2
+		//decide who to run on the CPU for child processes
+		//change priority if a different process is going to run
+		if(time_count == 0){
+			pid = findPID(execute[time_count].name);/*findPID to be implemented*/
+			param.sched_priority = 2;
+			if(sched_setscheduler(pid, SCHED_FIFO, & param) != 0) {
+	    		perror("sched_setscheduler error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if(execute[time_count].name != execute[time_count - 1].name){
+			pid_t prev_pid = findPID(execute[time_count - 1].name);
+			pid = findPID(execute[time_count].name);/*findPID to be implemented*/
 
-		最後把MAIN自己設成3
+			param.sched_priority = 1;
+			if(sched_setscheduler(prev_pid, SCHED_FIFO, & param) != 0) {
+	    		perror("sched_setscheduler error");
+				exit(EXIT_FAILURE);  
+			}
 
+			param.sched_priority = 2;
+			if(sched_setscheduler(pid, SCHED_FIFO, & param) != 0) {
+	    		perror("sched_setscheduler error");
+				exit(EXIT_FAILURE);  
+			}
+		}
 
-		main進入下一次for
+		volatile unsigned long i;
+		for(i=0;i<1000000UL;i++); 
 	}
 }
